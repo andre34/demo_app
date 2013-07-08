35f8ac50969b9cf05fc9c3932375d9bdf1edf800
andre34 <andrey34rus@bk.ru>
2013-07-09 00:56:57 +0400

    1 commit
    Signed-off-by: andre34 <andrey34rus@bk.ru>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '97f304d27347f7af30c0940cb2cc5d63b5b0d385c09c2ac643dbf3fb4f1feb6abd1549370b85f011adb2c0354bad106396e414e1c1d925e26c6f8305148159b8'
